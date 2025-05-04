83ebb5ef12132190266b9cd954159f5d8987bf47
iamlukovkin <iamlukovkin@gmail.com>
2025-05-05 02:18:58 +0300

    Optimize imports

--- src/main/java/com/geo/entity/Feature.java
+++ src/main/java/com/geo/entity/Feature.java
@@ -1,13 +1,10 @@
 package com.geo.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @Entity
