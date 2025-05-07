99ba3b3c32351c520c837a7740068c0f503db523
iamlukovkin <iamlukovkin@gmail.com>
2025-05-07 20:29:52 +0300

    Change CORS

--- src/main/java/com/geo/config/GeoSurfWebMvcConfiguration.java
+++ src/main/java/com/geo/config/GeoSurfWebMvcConfiguration.java
@@ -10,9 +10,8 @@ public class GeoSurfWebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
